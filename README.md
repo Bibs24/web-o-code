ab39718337a1818f0c91ee6edbc2a8e2e8336f56
Bibs24 <bibhuprasadsaha4@gmail.com>
2021-06-27 09:44:30 +0530

    Fourth Commit

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Embedded Conquerers
 Functionalities:
 Owl Carousel in header
 JavaScipt is used to make it functional.
+Still, moderation needed!
 
 IMP: Runs in local machine smoothly!
--- main.js
+++ main.js
@@ -1,16 +1,60 @@
-$('.home-slider').owlCarousel({
-    items:1,
-    nav:true,
-    dots:false,
-    autoplay:true,
-    autoplayTimeout:3000,
-    loop:true
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 68){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+    /**dark**/
+    const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
 });
 
-$('.small-image img').click(function(){
-
-    $(this).addClass('image-active').siblings().removeClass('image-active');
-    let image = $(this).attr('src');
-    $('.big-image img').attr('src', image);
-
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
 });
+    /**dark**/
+
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        loop:true
+    });
