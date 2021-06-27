36642e4226782d356e4e68f9291e8e4939d3bef8
Bibs24 <bibhuprasadsaha4@gmail.com>
2021-06-27 09:50:16 +0530

    Fourth Commit

--- main.js
+++ main.js
@@ -31,6 +31,38 @@ $(document).ready(function(){
         });
 
     });
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        loop:true
+    });
+
+    $('.small-image img').click(function(){
+
+        $(this).addClass('image-active').siblings().removeClass('image-active');
+        let image = $(this).attr('src');
+        $('.big-image img').attr('src', image);
+
+    });
+
+    $('.gallery .btn').click(function(){
+
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.gallery .box').show(400);
+        }else{
+            $('.gallery .box').not('.'+filter).hide(200);
+            $('.gallery .box').filter('.'+filter).show(400);
+        }
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
+
+    });
+
+});
     /**dark**/
     const chk = document.getElementById('chk');
 
@@ -50,11 +82,4 @@ close_btn.addEventListener('click', () => {
 });
     /**dark**/
 
-    $('.home-slider').owlCarousel({
-        items:1,
-        nav:true,
-        dots:false,
-        autoplay:true,
-        autoplayTimeout:3000,
-        loop:true
-    });
+   
